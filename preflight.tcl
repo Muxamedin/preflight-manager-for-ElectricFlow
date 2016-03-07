a117c6aef4089335230666dc2957705641335c81
bmikhail <bmikhail@electric-cloud.com>
2016-03-07 03:58:35 +0200

    procedures

--- preflight.tcl
+++ preflight.tcl
@@ -5,8 +5,24 @@ close          $fp
 package require tdom
 # parcer for xml - create DOM on doc 
 dom parse  $xml doc
-#get root element for doc
+
+# get root element for doc
 $doc documentElement root
+
+# create data structure 
+array set xmlArr [ list doc $doc root $root ]
+
+
+proc getSCM {root xmlArr } {
+
+}
+proc getServer {root xmlArr } {
+
+}
+
+proc getProcedure {root xmlArr } {
+
+}
 
 if { [ $root hasChildNodes ] } {
 	foreach item [ $root childNodes ] {
